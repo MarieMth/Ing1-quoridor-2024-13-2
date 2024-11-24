d1a65faf11237d0d0732cf712a6a83efcb788a32
sorayakettela <soraya.kettela@edu.ece.fr>
2024-11-24 12:18:17 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,6 @@
 #include <stdio.h>
+void position_pions (char plateau[LIGNES][COLONNES], int nb_joueurs);
+void caractere_pion (int nb_joueur, char* car_pion);
+void gotoligcol(int lig, int col);
+void Color(int couleurDuTexte,int couleurDeFond);
+void couleur();
