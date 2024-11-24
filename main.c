dd0e4105d9e4d654b1b243619ce129d5872c1150
sorayakettela <soraya.kettela@edu.ece.fr>
2024-11-24 22:04:50 +0100

    Update main.c

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 
 void positionPions (char plateau[17][17], int nb_joueurs);
 void caracterePion (int nb_joueur, char* carPion);
+void saisirPseudo(int nb_joueurs);
 void gotoligcol(int lig, int col);
 void Color(int couleurDuTexte,int couleurDeFond);
 void couleur();
