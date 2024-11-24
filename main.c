6525b7ef1ea0cadb1f3e89222857a73565d7a88b
MarieMth <marie.mathieu@edu.ece.fr>
2024-11-24 12:35:16 +0100

    Update main.c

--- main.c
+++ main.c
@@ -13,6 +13,21 @@ int validerPositionBarrieres(char plateau[17][17], Position barriere);
 void enregistrerBarrieres(char plateau[17][17], char plateauPrec[17][17], Position barriere);
 int estTableauIdentique(char plateau1[17][17], char plateau2[17][17]);
 void jouerTour(char nom[4][50], int numeros[4], int nbJoueurs);
+int Menu (choixDuJoueur, nbJoueurs); 
+int menuPartie(choixTour); 
+void passerTour(etatJeu *etatJeu); 
+void annulerCoup (etatJeu *positionPion, dernier Coup); 
 
+////////////////////////////
+// Nom de la Structure : etatJeu 
+// Role : tient compte de l'état du Jeu
+///////////////////////////
+typedef struct {
+        int joueurActuel; // Indice du joueur qui joue (0 pour le premier)
+        int nbJoueurs; // nombre total de joueurs définis en début de partie
+        char type; // 'D' pour un déplacement d'un joueur et 'B' pour le placement d'une barrière
+        int plateau [8][8];
+        int plateauActuel;
+    } etatJeu;
 #endif //GESTIONJEU_H
 
