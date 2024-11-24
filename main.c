f71866a3699a592dd135e3147cb79951f832f79b
sorayakettela <soraya.kettela@edu.ece.fr>
2024-11-24 14:07:01 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #ifndef AFFICHAGE_H
 #define AFFICHAGE_H
 
-void position_pions (char plateau[LIGNES][COLONNES], int nb_joueurs);
+void position_pions (char plateau[17][17], int nb_joueurs);
 void caractere_pion (int nb_joueur, char* car_pion);
 void gotoligcol(int lig, int col);
 void Color(int couleurDuTexte,int couleurDeFond);
