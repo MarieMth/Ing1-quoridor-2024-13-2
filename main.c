4be222f41c2a0ecad9f46fd5be910828b0cfe168
sorayakettela <soraya.kettela@edu.ece.fr>
2024-11-24 12:31:09 +0100

    Update main.c

--- main.c
+++ main.c
@@ -13,5 +13,5 @@ int validerPositionBarrieres(char plateau[17][17], Position barriere);
 void enregistrerBarrieres(char plateau[17][17], char plateauPrec[17][17], Position barriere);
 int estTableauIdentique(char plateau1[17][17], char plateau2[17][17]);
 
-#endif //GESTION_H
+#endif //GESTIONJEU_H
 
