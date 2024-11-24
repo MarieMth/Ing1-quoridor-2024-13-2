2f9cabd4b510356a0ab527fe2568013f427ff391
MarieMth <marie.mathieu@edu.ece.fr>
2024-11-24 13:45:10 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #ifndef SAUVEGARDE_H
 #define SUAVEGARDE_H
-
-
-
+void sauvegardeScores (const char *fichierScoresJoueurs); 
+void chargementScores (const char *fichierScores Joueurs); 
+void miseAJourScores (const char *fichierScoresJoueurs, pointsGagnes, gagne); 
 #endif //SAUVEGARDE_H
 
 
