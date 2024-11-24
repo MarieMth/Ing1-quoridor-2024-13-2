585091a5999ccbbc1d861a9f60c16e472f90b6e9
gathe78 <agathe.dersu@edu.ece.fr>
2024-11-24 12:33:23 +0100

    Update main.c

--- main.c
+++ main.c
@@ -12,6 +12,7 @@ void recopierTableau(char plateauARecopier[17][17], char plateau[17][17]);
 int validerPositionBarrieres(char plateau[17][17], Position barriere);
 void enregistrerBarrieres(char plateau[17][17], char plateauPrec[17][17], Position barriere);
 int estTableauIdentique(char plateau1[17][17], char plateau2[17][17]);
+void jouerTour(char nom[4][50], int numeros[4], int nbJoueurs);
 
 #endif //GESTIONJEU_H
 
